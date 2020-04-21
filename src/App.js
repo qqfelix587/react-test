50cdf628e6a063453e5e2226e14ae820875e5867
Yongseok <qqfelix@naver.com>
2020-04-21 18:57:49 +0900

    배열에 항목추가하기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React, {useRef} from 'react';
+import React, {useRef,useState} from 'react';
 import Hello from './Hello'
 import './App.css'
 import Wrapper from './Wrapper';
 import Counter from './Counter';
 import InputSample from './InputSample';
 import UserList from './UserList';
+import CreateUser from './CreatUser';
+
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  
+  });
+  const {username, email} = inputs;
+  const onChange = e =>{
+    const {name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+  const [users, setUsers] = useState([
+    
+
     {
         id: 1,
         username: 'velopert',
@@ -24,9 +41,26 @@ function App() {
     }
 
 
-];
+]);
+
+
 const nextId = useRef(4);
 const onCreate = () => {
+  const user = {
+    id: nextId.current,
+    username,
+    email
+  };
+  // setUsers(users.push(user));
+  setUsers([...users, user]);
+  // setUsers(users.concat(user)); 이렇게 concat을 사용해도됨. 
+  // 하지만 push등을 사용하면 안됨.
+  setInputs({
+    username : '',
+    email: '',
+  });
+
+  
   console.log(nextId.current); //4
   nextId.current +=1;
   // 다시 새로운 값을 가리키도록
@@ -35,8 +69,14 @@ const onCreate = () => {
 }
 // 값이 reredering 되더라도 값을 계속해서 기억하고 있을 수 있도록 하기 위해 사용.
   return (
-    <UserList users={users}/>
-
+    <>
+    <CreateUser 
+      username={username} 
+      email={email} 
+      onChange={onChange} 
+      onCreate = {onCreate}/>
+     <UserList users={users}/>
+    </>
   );
 }
 
