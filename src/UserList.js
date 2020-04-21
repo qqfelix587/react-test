9431859e751e590534a9f0cb8ada956b0dde1cc9
Yongseok <qqfelix@naver.com>
2020-04-21 22:51:09 +0900

    use Effect를 사용

--- src/UserList.js
+++ src/UserList.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, {useEffect}from 'react';
 
 function User({user, onRemove, onToggle}){
     const {username, email, id, active} = user;
+
+    // useEffect(()=> {
+    //     console.log('컴포넌트가 화면에 나타남');
+    //     // mount 될 때 주로 하는 작업
+    //     // props로 받은 값을 컴포넌트의 state로 설정하는 것.
+    //     // 외부 API 요청할 때
+    //     // D3 Video.js
+    //     // setInterval, setTimeout
+
+    //     return () =>{
+    //         // unmount 될 때 주로 하는 작업
+    //         // clearInterval, clearTimeout
+    //         // 라이브러리 인스턴스 제거.
+    //         console.log('컴포넌트가 화면에서 사라짐!!');
+    //     }
+    // }, []);
+
+    useEffect(()=>{
+        console.log('user 값이 설정됨');
+        console.log(user);
+        // 특정값이 update되고 난 직후에 실행됨.
+        
+        return () =>{
+            console.log('user 값이 바뀌기 전')
+            console.log(user);
+        }
+
+        },[user]
+        // []에 어떤 값을 넣으면 해당값이 바뀔 때 마다 호출/ 처음 나타날 때도 호출됨.
+        // useEffect 내에서 사용하고 있는 값이 배열에 들어가 있지 않으면, 에러 출력됨.
+        );
+
+    // useEffect (( )=>{
+    //     console.log(user);
+
+    // })
+    // 모든 값이 변화가 있을 때마다 실행됨.
+
+
+
+
+    // deps 라는 배열은 의존되는 값들을 넣어주는 것, 
+    // 이 값이 비어있으면, component가 처음 화면에 나타날 때만 실행됨
     return (
         <div>
             <b style = {{
@@ -10,7 +53,8 @@ function User({user, onRemove, onToggle}){
 
             }}
             onClick={()=>onToggle(id)}
-            >{username}</b>
+            >{username}
+            </b>
             &nbsp;
             <span>({email})</span>
             <button onClick={() => onRemove(id)}>삭제</button>
