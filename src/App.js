29f357a5ae1f54c3109e22354b7b067ca9efdacf
Yongseok <qqfelix@naver.com>
2020-04-23 11:22:50 +0900

    use reducer 기초

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React, {useRef,useState, useMemo, useCallback} from 'react';
+// import React, {useRef,useState, useMemo, useCallback} from 'react';
+import React, {useRef,useReducer, useMemo, useCallback} from 'react';
+
+
 import Hello from './Hello'
 import Wrapper from './Wrapper';
 import Counter from './Counter';
@@ -10,25 +13,13 @@ function countActiveUsers(users){
   console.log('활성 사용자 수를 세는중...');
   return users.filter(user => user.active).length;
 };
-function App() {
-  const [inputs, setInputs] = useState({
+
+const initialState = {
+  inputs: {
     username: '',
     email: '',
-  
-  });
-
-  const {username, email} = inputs;
-
-
-  const onChange = useCallback(e =>{
-    const {name, value} = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value
-    });
-  }, [inputs]);
-
-  const [users, setUsers] = useState([
+  },
+  users: [
     {
         id: 1,
         username: 'velopert',
@@ -49,61 +40,195 @@ function App() {
         active: false,
 
     }
-]);
+]
+}
+// useState를 useReducer 로 대체하기 위한 과정 진행
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CHANGE_INPUT":
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.name]: action.value
+          // 불변성 유지를 위함.
+        }
+      }
+    case 'CREATE_USER':
+      return {
+          inputs: initialState.inputs,
+          users: state.users.concat(action.user)
+          // 기존과 달리 input을 업데이트하고, users를 생성하는 작업을 동시에 진행함.
+      }
+
+    case 'TOGGLE_USER':
+      return {
+        ...state,
+        users: state.users.map(user => 
+          user.id ===action.id 
+          ? {...user, active: !user.active}
+          :user) 
+      }
+      case 'REMOVE_USER':
+        return {
+          ...state,
+          users:state.users.filter(
+            user =>
+            user.id !==action.id
+             
+            )
+        }
+      
+    default: 
+      throw new Error('Unhandled action');
+  }
+}
 
 
-const nextId = useRef(4);
 
-const onCreate = useCallback(() => {
-  const user = {
-    id: nextId.current,
-    username,
-    email
-  };
-  // setUsers(users.push(user));
-  setUsers(users => [...users, user]);
-  // 이렇게 되면 항상 최신 users를 조회하게 되어 users가 업데이트 될때마다 rerendering이 되지않음
-  // setUsers(users.concat(user)); 이렇게 concat을 사용해도됨. 
-  // 하지만 push등을 사용하면 안됨.
-  setInputs({
-    username : '',
-    email: '',
-  });
-  // console.log(nextId.current); //4
-  nextId.current +=1;
-  // 다시 새로운 값을 가리키도록
-},[username, email]);
-// 값이 reredering 되더라도 값을 계속해서 기억하고 있을 수 있도록 하기 위해 사용.
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const nextId = useRef(4);
+  const {users} = state;
+  const {username, email} = state.inputs;
+
+  const onChange = useCallback(e => {
+    const {name, value} = e.target
+    dispatch({
+      type: 'CHANGE_INPUT',
+      name,
+      value
+    })
+  }, []);
+  const onCreate = useCallback(() => {
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id:nextId.current,
+        username,
+        email
+      }
+    });
+    nextId.current +=1;
+  },[username, email])
+  
+  const onToggle = useCallback(id => {
+    dispatch({
+      type: 'TOGGLE_USER',
+      id
+    });
+  },[]);
+  const onRemove = useCallback(
+    id => {
+    dispatch({
+      type: 'REMOVE_USER',
+      id
+    });
+  }, []);
+  const count = useMemo(() => countActiveUsers(users),[users]);
 
 
-const onRemove = useCallback (id => {
-  setUsers(users => users.filter(user => user.id !== id));
-},[]);
+//   const [inputs, setInputs] = useState({
+//     username: '',
+//     email: '',
+  
+//   });
+
+//   const {username, email} = inputs;
+
+
+//   const onChange = useCallback(e =>{
+//     const {name, value} = e.target;
+//     setInputs({
+//       ...inputs,
+//       [name]: value
+//     });
+//   }, [inputs]);
+
+//   const [users, setUsers] = useState([
+//     {
+//         id: 1,
+//         username: 'velopert',
+//         email: 'public.velopert@naver.com',
+//         active: true,
+//     },
+//     {
+//         id: 2,
+//         username: 'veefefelopert',
+//         email: 'public.eefeefvelopert@naver.com',
+//         active: false,
+
+//     },
+//     {
+//         id: 3,
+//         username: 'velasgdawegvopert',
+//         email: 'public.efwsafefsdvvdvelopert@naver.com',
+//         active: false,
+
+//     }
+// ]);
+
+
+// const nextId = useRef(4);
+
+// const onCreate = useCallback(() => {
+//   const user = {
+//     id: nextId.current,
+//     username,
+//     email
+//   };
+//   // setUsers(users.push(user));
+//   setUsers(users => [...users, user]);
+//   // 이렇게 되면 항상 최신 users를 조회하게 되어 users가 업데이트 될때마다 rerendering이 되지않음
+//   // setUsers(users.concat(user)); 이렇게 concat을 사용해도됨. 
+//   // 하지만 push등을 사용하면 안됨.
+//   setInputs({
+//     username : '',
+//     email: '',
+//   }); 
+//   // console.log(nextId.current); //4
+//   nextId.current +=1;
+//   // 다시 새로운 값을 가리키도록
+// },[username, email]);
+// // 값이 reredering 되더라도 값을 계속해서 기억하고 있을 수 있도록 하기 위해 사용.
+
+
+// const onRemove = useCallback (id => {
+//   setUsers(users => users.filter(user => user.id !== id));
+// },[]);
  
 
-const onToggle = useCallback(id => {
-  setUsers(users => users.map(
-    user => user.id === id
-    ? {...user, active: !user.active}
-    : user
-  ));
+// const onToggle = useCallback(id => {
+//   setUsers(users => users.map(
+//     user => user.id === id
+//     ? {...user, active: !user.active}
+//     : user
+//   ));
 
-}, []);
-// const count = countActiveUsers(users);
-// 이렇게 사용하면 rerendering이 될 때마다 실행됨. -> useMemo 사용함(특정 값이 바뀔 때만 동작)
-const count = useMemo(() => countActiveUsers(users), [users]);
-//  deps 배열 안에 넣는 값이 바뀔 때만 연산 진행됨. / 성능 최적화시키기 위해 적용하는 것.
+// }, []);
+// // const count = countActiveUsers(users);
+// // 이렇게 사용하면 rerendering이 될 때마다 실행됨. -> useMemo 사용함(특정 값이 바뀔 때만 동작)
+// const count = useMemo(() => countActiveUsers(users), [users]);
+// //  deps 배열 안에 넣는 값이 바뀔 때만 연산 진행됨. / 성능 최적화시키기 위해 적용하는 것.
 
 
   return (
     <>
-    <CreateUser 
-      username={username} 
-      email={email} 
-      onChange={onChange} 
-      onCreate = {onCreate}/>
-      <UserList users={users} onRemove={onRemove} onToggle = {onToggle}/>
-      <div>활성 사용자 수 : {count}</div>
+    <CreateUser username={username} 
+    email={email} 
+    onChange={onChange}
+    onCreate={onCreate}
+      // username={username} 
+      // email={email} 
+      // onChange={onChange} 
+      // onCreate = {onCreate}
+      
+      />
+      {/* <UserList users={users} onRemove={onRemove} onToggle = {onToggle}/> */}
+      <UserList users = {users} onToggle={onToggle} onRemove={onRemove}/>
+      {/* <div>활성 사용자 수 : {count}</div> */}
+  <div> 활성 사용자 수: {count}}</div>
     </> 
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,39 @@
-import React, {useState} from 'react';
+// import React, {useState} from 'react';
+import React, {useReducer} from 'react';
+// 기존의 useState로 동적 상태 변화를 제어하는 코드를 useReducer로 해보려 함.
+
+function reducer(state, action){
+    // state의 type은 어떤 것이든 상관없음
+    switch (action.type){
+        case 'INCREMENT':
+            return state + 1;
+
+        case 'DECREMENT':
+            return state - 1;
+        default: 
+            throw new Error('Unhandled action');
+            // return state로 해도 됨. -> 준비하지 않은 action이 올 경우 아무 변화도 없게 할 때,
+    }
+}
+
 
 function Counter(){
-    const [number, setNumber] = useState(0);
+
+    const [number, dispatch] = useReducer(reducer, 0);
+    //   현재의 상태, action을 발생시키는 함수
 
     const onIncrease = () => {
-        setNumber(prevNumber => prevNumber + 1);
+        // setNumber(prevNumber => prevNumber + 1);
+        dispatch({
+            type: 'INCREMENT'
+        });
+
     }
     const onDecrease = () => {
-        setNumber(number-1);
-
+        // setNumber(number-1);
+        dispatch({
+            type: 'DECREMENT'
+        });
     }
 
     return <div>
